91cec379cf42f4afc18a5531c8ba4e6416b075b8
Kevin Miller <kevinjmiller@google.com>
2026-03-07 14:23:55 +0000

    Attempt to fix a lint error

--- disentangled_rnns/library/disrnn_test.py
+++ disentangled_rnns/library/disrnn_test.py
@@ -127,9 +127,10 @@ class DisrnnTest(absltest.TestCase):
     # The first output_size elements are the predicted targets, andt the last
     # element is the penalty
     self.assertEqual(network_outputs.shape, (n_sessions, n_trials, 3))
-    self.assertEqual(network_states.shape, (n_sessions,
-                                            n_trials,
-                                            self.disrnn_config.latent_size))
+    self.assertEqual(
+      network_states.shape, 
+      (n_sessions, n_trials, self.disrnn_config.latent_size),
+    )
 
   def test_disrnn_trainable(self):
     """Smoke test to check that the disRNN can be trained."""
