ad7cb5100d862e421f07fc8dbad20a58141eecde
Kevin Miller <kevinjmiller@google.com>
2026-03-07 14:37:27 +0000

    Attempt to fix lint errors

--- disentangled_rnns/library/disrnn_test.py
+++ disentangled_rnns/library/disrnn_test.py
@@ -128,8 +128,8 @@ class DisrnnTest(absltest.TestCase):
     # element is the penalty
     self.assertEqual(network_outputs.shape, (n_sessions, n_trials, 3))
     self.assertEqual(
-      network_states.shape, 
-      (n_sessions, n_trials, self.disrnn_config.latent_size),
+        network_states.shape, 
+        (n_sessions, n_trials, self.disrnn_config.latent_size),
     )
 
   def test_disrnn_trainable(self):
